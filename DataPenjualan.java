bb980749a99d2bac0e0dbe6eb673c47d5dcbace5
mohammadarfian <91176224+mohammadarfian@users.noreply.github.com>
2022-05-29 15:33:43 +0700

    Create DataPenjualan.java

---
+++ DataPenjualan.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DataPenjualan {
+
+    public int id;
+    public String nama, keperluan;
+    public DataPenjualan next;
+
+    static Scanner in = new Scanner(System.in);
+    static Scanner str = new Scanner(System.in);
+
+    public void input(){
+        System.out.print("Masukkan id           : ");
+        id= in.nextInt();
+        System.out.print("Masukkan nama         : ");
+        nama=str.nextLine();
+        System.out.print("Masukkan keperluan    : ");
+        keperluan=str.nextLine();
+        next=null;
+    }
+
+    public void read(){
+
+        System.out.println("|| "+id+" || "+nama+" || "+keperluan+" ||");
+
+    }
+}
